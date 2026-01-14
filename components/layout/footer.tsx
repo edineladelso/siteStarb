5ef49eb2e56a2a4d237591f162b68cafd4f19880
edineladelso <marengulae@gmail.com>
2026-01-15 01:50:34 +0200

    Remover arquivos de exemplo e documentação desatualizados; atualizar o rodapé para usar links do Next.js

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Instagram, Github, Mail } from "lucide-react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -10,34 +11,34 @@ export default function Footer() {
     "
     >
       <div className="max-w-sm w-full md:w-auto">
-        <Image
-          src="/img/starbdarkFinal.webp"
-          alt="Logo da Star B"
-          width={130}
-          height={50}
-          className="rounded-xl bg-amber-50 p-1.5 w-28 md:w-32 h-auto"
-        />
-        <p className="mt-2 font-spectral text-sm md:text-sm">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
-          voluptatibus ea vitae debitis eius reprehenderit dolor adipisci
-        </p>
+      <Image
+        src="/img/starbdarkFinal.webp"
+        alt="Logo da Star B"
+        width={130}
+        height={50}
+        className="rounded-xl bg-amber-50 p-1.5 w-28 md:w-32 h-auto"
+      />
+      <p className="mt-2 font-spectral text-sm md:text-sm">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
+        voluptatibus ea vitae debitis eius reprehenderit dolor adipisci
+      </p>
       </div>
       <ul className="flex gap-4 items-center">
-        <li>
-          <a href="#">
-            <Instagram className="cursor-pointer hover:text-blue-400 w-6 h-6 md:w-5 md:h-5"/>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <Github className="cursor-pointer hover:text-blue-400 w-6 h-6 md:w-5 md:h-5"/>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <Mail className="cursor-pointer hover:text-blue-400 w-6 h-6 md:w-5 md:h-5"/>
-          </a>
-        </li>
+      <li>
+        <Link href="#">
+        <Instagram className="cursor-pointer hover:text-blue-400 w-6 h-6 md:w-5 md:h-5"/>
+        </Link>
+      </li>
+      <li>
+        <Link href="#">
+        <Github className="cursor-pointer hover:text-blue-400 w-6 h-6 md:w-5 md:h-5"/>
+        </Link>
+      </li>
+      <li>
+        <Link href="#">
+        <Mail className="cursor-pointer hover:text-blue-400 w-6 h-6 md:w-5 md:h-5"/>
+        </Link>
+      </li>
       </ul>
     </footer>
   );
