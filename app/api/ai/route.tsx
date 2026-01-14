d354ea6d75ffc95f71d9d77c2e87259c29b9751c
edineladelso <marengulae@gmail.com>
2026-01-14 18:12:50 +0200

    Reorganizando ai para deploy

---
+++ app/api/ai/route.tsx
@@ -0,0 +1,6 @@
+// app/api/ai/route.ts
+
+export async function GET(request: Request) {
+  return new Response('Hello, Vercel!');
+}
+// Ou qualquer outra função handler (POST, PUT, DELETE) que você esteja usando.
