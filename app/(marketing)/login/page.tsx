7ba3299f80644c98d57ed85b56cdceedf246ec35
edineladelso <marengulae@gmail.com>
2026-01-18 19:42:32 +0200

    login comentadao

--- app/(marketing)/login/page.tsx
+++ app/(marketing)/login/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@/components/ui/button";
-import {  registrarComEmail, fazerLoginComEmail} from "@/lib/supabase/auth/auth-actions";
+import {
+  registrarComEmail,
+  fazerLoginComEmail,
+} from "@/lib/supabase/auth/auth-actions";
 
 export default function LoginPage() {
   return (
@@ -8,8 +11,8 @@ export default function LoginPage() {
       <input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <input id="password" name="password" type="password" required />
-      <Button formAction={fazerLoginComEmail}>Log in</Button>
-      <Button formAction={registrarComEmail}>Sign up</Button>
+      {/* <Button formAction={fazerLoginComEmail}>Log in</Button>
+      <Button formAction={registrarComEmail}>Sign up</Button> */}
     </form>
   );
 }
