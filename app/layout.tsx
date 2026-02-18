cb8fcdf2099483e109ae2f476f8a485818b15502
edineladelso <marengulae@gmail.com>
2026-02-18 14:26:30 +0200

    atualizacao pronta

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       className={`${mavenPro.variable} ${poppins.variable} ${muktaMalar.variable} ${montserrat.variable} ${neuton.variable} ${spectral.variable}`}
     >
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <body className="bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50 min-h-svh">
+      <body className="min-h-svh bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50">
         <div className="min-h-svh">{children}</div>
       </body>
     </html>
